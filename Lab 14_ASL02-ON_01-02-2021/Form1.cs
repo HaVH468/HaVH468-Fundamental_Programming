7cbc9ef64fa52c90fac3731f2d33aa28e643b5c6
HaVH468 <72876762+HaVH468@users.noreply.github.com>
2021-02-05 15:42:42 +0100

    Add files via upload

---
+++ Lab 14_ASL02-ON_01-02-2021/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_14_ASL02_ON_01_02_2021
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result;
+            if (CheckPrime(Convert.ToInt32(textBox1.Text)) == true) 
+            {
+                result = "This number is prime number";
+            }
+            else
+            {
+                result = "This number is not prime number";
+            }
+            listBox1.Items.Add(result);
+        }
+        public bool CheckPrime(int x)
+        {
+            for (int i = 2; i < x; i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            listBox1.Items.Clear();
+        }
+    }
+}
---
+++ Lab 14_ASL02-ON_01-02-2021/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_14_ASL02_ON_01_02_2021
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sum = int.Parse(textBox1.Text) + int.Parse(textBox2.Text);
+            listBox1.Items.Add(sum);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int sub = int.Parse(textBox1.Text) - int.Parse(textBox2.Text);
+            listBox1.Items.Add(sub);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int mul = int.Parse(textBox1.Text) - int.Parse(textBox2.Text);
+            listBox1.Items.Add(mul);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int div = int.Parse(textBox1.Text) / int.Parse(textBox2.Text);
+            listBox1.Items.Add(div);
+        }
+    }
+}
