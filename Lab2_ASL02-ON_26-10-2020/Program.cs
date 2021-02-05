c8823327750610edc2936ba05b96504f79af724c
HaVH468 <72876762+HaVH468@users.noreply.github.com>
2021-02-05 15:27:59 +0100

    Add files via upload

---
+++ Lab2_ASL02-ON_26-10-2020/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            int x, y, result;
+            Console.Write("Enter the first number: ");
+            x = Convert.ToInt32(Console.Read());
+            Console.Write("Enter the second number: ");
+            y = Convert.ToInt32(Console.Read());
+            Console.WriteLine($"Sum = {result = x + y}");
+            Console.WriteLine($"Subtract = {result = x - y}");
+            Console.WriteLine($"Multiple = {result = x * y}");
+            Console.WriteLine($"Divide = {result = x / y}");
+
+            //2
+            Console.Write("Enter the number: ");
+            x = Convert.ToInt32(Console.Read());
+            Console.WriteLine($"Multiplication table of {x} ");
+            for (int i = 0; i <= x; i++)
+            {
+                Console.WriteLine(result = x * i);
+            }
+
+            //3
+            bool re;
+            Console.Write("Enter the first number: ");
+            x = Convert.ToInt32(Console.Read());
+            Console.Write("Enter the second number: ");
+            y = Convert.ToInt32(Console.Read());
+            if (x < 0 && y > 0 || x > 0 && y < 0)
+            {
+                Console.WriteLine(re = true);
+            }
+            else
+            {
+                Console.WriteLine(re = false);
+            }
+
+            //4
+            Console.Write("Enter the number: ");
+            x = Convert.ToInt32(Console.Read());
+            if(20 <= x && x <= 100)
+            {
+                Console.WriteLine("The given integer is within 20 of 100");
+            }
+            else
+            {
+                Console.WriteLine("The given integer is not within 20 of 100");
+            }
+
+            //5
+            for (int i = 1; i <= 99; i++)
+            {
+                if (i%2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Lab2_ASL02-ON_26-10-2020/Program1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            int x;
+            Console.Write("Enter the number: ");
+            x = Convert.ToInt32(Console.Read());
+            if (x < 0)
+            {
+                Console.WriteLine("The number is not positive");
+            }
+            else if(x % 3 == 0)
+            {
+                Console.WriteLine("the number is a multiple of 3");
+            }else if(x % 7 == 0)
+            {
+                Console.WriteLine("the number is a multiple of 7");
+            }
+
+            //2
+            int y;
+            Console.Write("Enter the 1st number: ");
+            x = Convert.ToInt32(Console.Read());
+            Console.Write("Enter the 2nd number: ");
+            y = Convert.ToInt32(Console.Read());
+            if (x > -10 && x < 10 || y > -10 && y < 10)
+            {
+                Console.WriteLine("An integer (from the two given integers) is in the range -10 to 10");
+            }
+
+            //3
+            if (x < 100 && y > 200 || x > 200 && y < 100)
+            {
+                Console.WriteLine("true");
+            }
+
+            //4
+            double p, a;
+            Console.Write("Enter the radius of a circle: ");
+            x = Convert.ToInt32(Console.Read());
+            Console.WriteLine($"the perimeter {p = 2 * x * 3.14}");
+            Console.WriteLine($"the area {a = x * x * 3.14}");
+
+            //5
+            double v;
+            Console.Write("Enter distance: ");
+            x = Convert.ToInt32(Console.Read());
+            Console.Write("Enter time: ");
+            y = Convert.ToInt32(Console.Read());
+            Console.WriteLine($"the speed in kilometers per hour {v = x/y}");
+
+            //6
+            Console.Write("Enter the radius of a sphere: ");
+            x = Convert.ToInt32(Console.Read());
+            Console.WriteLine($"the area {a = 4/3 * 3.14 * x*x*x}");
+
+            //7
+            bool result = true;
+            Console.Write("Enter the 1st number: ");
+            x = Convert.ToInt32(Console.Read());
+            Console.Write("Enter the 2nd number: ");
+            y = Convert.ToInt32(Console.Read());
+            if (x % 2 == 0 && y % 2 == 0)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(result = false);
+            }
+        }
+    }
+}
